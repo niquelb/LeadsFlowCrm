13b4f5b2c0407288c6944b95befec418a0ab8856
N P <nicolas.palaorocort03@gmail.com>
2023-05-07 16:54:30 +0200

    Made Id field have a default value
    Made the Id field in the User model automatically generate a GUID so we don't have to do it manually (can be overrriden)

--- DataAccess/Models/User.cs
+++ DataAccess/Models/User.cs
@@ -8,7 +8,7 @@ namespace DataAccess.Models;
 
 public class User
 {
-    public String Id { get; set; }
+    public String Id { get; set; } = Guid.NewGuid().ToString();
     public String OAuthToken { get; set; }
     public String Email { get; set; }
     public String DisplayName { get; set; }
