2e058fb0759e3492237ccf4f2d466a28ed3aaabc
N P <nicolas.palaorocort03@gmail.com>
2023-05-07 17:14:03 +0200

    Created Organization model

---
+++ DataAccess/Models/Organization.cs
@@ -0,0 +1,8 @@
+﻿namespace DataAccess.Models;
+
+public class Organization
+{
+	public String Id { get; set; } = Guid.NewGuid().ToString();
+	public String Name { get; set; }
+    public String? CreatorId { get; set; }
+}
