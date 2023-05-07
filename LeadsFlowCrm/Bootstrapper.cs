de07d21f3ef9473b79e791c982c56b910d9cf2ca
N P <nicolas.palaorocort03@gmail.com>
2023-05-07 19:56:18 +0200

    Setup Caliburn.Micro for the WPF app

---
+++ LeadsFlowCrm/Bootstrapper.cs
@@ -0,0 +1,23 @@
+﻿using Caliburn.Micro;
+using LeadsFlowCrm.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace LeadsFlowCrm;
+
+internal class Bootstrapper : BootstrapperBase
+{
+    public Bootstrapper()
+    {
+        Initialize();
+    }
+
+	protected override void OnStartup(object sender, StartupEventArgs e)
+	{
+		DisplayRootViewForAsync<ShellViewModel>();
+	}
+}
--- LeadsFlowCrm/Views/ShellView.xaml.cs
+++ LeadsFlowCrm/Views/ShellView.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace LeadsFlowCrm
+namespace LeadsFlowCrm.Views
 {
 	/// <summary>
-	/// Interaction logic for MainWindow.xaml
+	/// Lógica de interacción para ShellView.xaml
 	/// </summary>
-	public partial class MainWindow : Window
+	public partial class ShellView : Window
 	{
-		public MainWindow()
+		public ShellView()
 		{
 			InitializeComponent();
 		}
