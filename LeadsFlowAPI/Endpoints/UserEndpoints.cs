bcad2651441a06c0cc9f139a1ac1f032342f905f
N P <nicolas.palaorocort03@gmail.com>
2023-05-07 17:04:41 +0200

    Fixed PutUser endpoint
    Fixed user updating issue that made it such that if you used an Id that didn't exist the API would return 200 even if no user was updated, it now returns a 404

--- LeadsFlowAPI/Endpoints/UserEndpoints.cs
+++ LeadsFlowAPI/Endpoints/UserEndpoints.cs
@@ -63,13 +63,13 @@ public static class UserEndpoints
 	/// </summary>
 	/// <param name="user">User to be inserted</param>
 	/// <param name="userDAO">DAO object from Dependency Injection</param>
-	/// <returns>Result wrapped in an HTTP response code</returns>
+	/// <returns>HTTP response code, if succesful, the ID of the inserted user is returned</returns>
 	private static async Task<IResult> PostUser(User user, IUserDAO userDAO)
 	{
 		try
 		{
 			await userDAO.InsertUser(user);
-			return Results.Ok();
+			return Results.Ok(user.Id);
 		}
 		catch (Exception ex)
 		{
@@ -82,13 +82,22 @@ public static class UserEndpoints
 	/// </summary>
 	/// <param name="user">User to be updated</param>
 	/// <param name="userDAO">DAO object from Dependency Injection</param>
-	/// <returns>Result wrapped in an HTTP response code</returns>
+	/// <returns>HTTP response code, if succesful, the ID of the updated user is returned</returns>
 	private static async Task<IResult> PutUser(User user, IUserDAO userDAO)
 	{
 		try
 		{
+			/*
+			 Before doing the updating we check if the user exists, this is done because otherwise
+			the API will return 200 even if the updating failed due to not being any user with that ID
+			 */
+			var result = await userDAO.GetUser(user.Id);
+			if (result == null)
+			{
+				return Results.NotFound(user.Id);
+			}
 			await userDAO.UpdateUser(user);
-			return Results.Ok();
+			return Results.Ok(user.Id);
 		}
 		catch (Exception ex)
 		{
