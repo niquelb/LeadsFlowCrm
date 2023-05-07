49149345a7019670a44fa84832446237e866ecc2
N P <nicolas.palaorocort03@gmail.com>
2023-05-07 20:15:54 +0200

    Implemented DI with SimpleContainer
    Implemented Dependency Injection using SimpleContainer to the application

--- LeadsFlowCrm/Bootstrapper.cs
+++ LeadsFlowCrm/Bootstrapper.cs
@@ -9,15 +9,55 @@ using System.Windows;
 
 namespace LeadsFlowCrm;
 
+/// <summary>
+/// Class responsible for setting up all the necessary backend for the app
+/// </summary>
 internal class Bootstrapper : BootstrapperBase
 {
+	// SimpleContainer is used for Dependency Injection
+	private SimpleContainer _container = new();
     public Bootstrapper()
     {
         Initialize();
     }
 
+	protected override void Configure()
+	{
+		// This instance will be returned whenever we "ask" for our Container
+		_container.Instance(_container);
+
+		_container
+			.Singleton<IWindowManager, WindowManager>()
+			.Singleton<IEventAggregator, EventAggregator>();
+
+		// Adds all of our ViewModels into Dependency Injection
+		GetType().Assembly.GetTypes()
+			.Where(t => t.IsClass)
+			.Where(t => t.Name.EndsWith("ViewModel"))
+			.ToList()
+			.ForEach(t => _container.RegisterPerRequest(t, t.ToString(), t));
+	}
+
 	protected override void OnStartup(object sender, StartupEventArgs e)
 	{
+		// Set "ShellView" as the startup window
 		DisplayRootViewForAsync<ShellViewModel>();
+	}
+
+	protected override object GetInstance(Type service, string key)
+	{
+		// Get the instance of a specified type using a key
+		return _container.GetInstance(service, key);
+	}
+
+	protected override IEnumerable<object> GetAllInstances(Type service)
+	{
+		// Get all instances of a specified type
+		return _container.GetAllInstances(service);
+	}
+
+	protected override void BuildUp(object instance)
+	{
+		_container.BuildUp(instance);
 	}
 }
