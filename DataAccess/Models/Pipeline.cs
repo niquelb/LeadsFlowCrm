40d7db08fcca59dc5da255a46f3bdc164781984b
N P <nicolas.palaorocort03@gmail.com>
2023-05-07 18:51:50 +0200

    Created Pipeline model

---
+++ DataAccess/Models/Pipeline.cs
@@ -0,0 +1,9 @@
+﻿namespace DataAccess.Models;
+
+public class Pipeline
+{
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+    public string Name { get; set; }
+    public string? CreatorId { get; set; }
+    
+}
