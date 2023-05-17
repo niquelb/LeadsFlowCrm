24071141211f1c15f81cd744b4a04e350242fa0b
N P <nicolas.palaorocort03@gmail.com>
2023-05-17 16:47:29 +0200

    Create EmailModel
    Created the model class for the emails retrieved from the Gmail API

---
+++ LeadsFlowCrm/Models/EmailModel.cs
@@ -0,0 +1,13 @@
+﻿namespace LeadsFlowCrm.Models;
+
+/// <summary>
+/// Model class for the emails retrieved from the mailing provider
+/// </summary>
+public class EmailModel
+{
+    public string Sender { get; set; }
+    public string SubjectLine { get; set; }
+    public string Body { get; set; }
+    public bool IsFavorite { get; set; }
+    public bool IsRead { get; set; }
+}
