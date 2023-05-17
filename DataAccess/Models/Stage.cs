de59d1a7d21df5e9d3a3efa27db139e9dca1d919
N P <nicolas.palaorocort03@gmail.com>
2023-05-17 22:28:53 +0200

    Created Stage model

---
+++ DataAccess/Models/Stage.cs
@@ -0,0 +1,10 @@
+﻿
+namespace DataAccess.Models;
+
+public class Stage
+{
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+    public string Name { get; set; }
+    public string? Color { get; set; }
+    public string PipelineId { get; set; }
+}
