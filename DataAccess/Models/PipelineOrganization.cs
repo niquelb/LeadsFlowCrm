5c08e48bb78ebe5a4e017d5b3287380c6543c516
N P <nicolas.palaorocort03@gmail.com>
2023-05-17 22:48:14 +0200

    Created PipelineOrganization model

---
+++ DataAccess/Models/PipelineOrganization.cs
@@ -0,0 +1,9 @@
+﻿
+namespace DataAccess.Models;
+
+public class PipelineOrganization
+{
+	public string Id { get; set; } = Guid.NewGuid().ToString();
+	public string PipelineId { get; set; }
+	public string OrganizationId { get; set; }
+}
