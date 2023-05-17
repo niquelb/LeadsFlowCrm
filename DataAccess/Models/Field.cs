b59522ba9381f172afea5045a84513341bd48095
N P <nicolas.palaorocort03@gmail.com>
2023-05-17 22:04:11 +0200

    Created Field model

---
+++ DataAccess/Models/Field.cs
@@ -0,0 +1,10 @@
+﻿
+namespace DataAccess.Models;
+
+public class Field
+{
+	public string Id { get; set; } = Guid.NewGuid().ToString();
+	public string Name { get; set; }
+    public string? Type { get; set; }
+    public string PipelineId { get; set; }
+}
