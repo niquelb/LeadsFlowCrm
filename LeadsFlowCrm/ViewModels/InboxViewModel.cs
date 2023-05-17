fad73e1596363382669a411233c885091bffc21f
N P <nicolas.palaorocort03@gmail.com>
2023-05-17 21:10:24 +0200

    Setup BindableCollection with sample data
    Setup a BindableCollection of type EmailModel for the InboxView with sample data

--- LeadsFlowCrm/ViewModels/InboxViewModel.cs
+++ LeadsFlowCrm/ViewModels/InboxViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Caliburn.Micro;
+using LeadsFlowCrm.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,4 +10,24 @@ namespace LeadsFlowCrm.ViewModels;
 
 public class InboxViewModel
 {
+	private BindableCollection<EmailModel> _emails = new();
+
+	public InboxViewModel()
+	{
+		Emails.Add(new EmailModel
+		{
+			Sender = "Mike",
+			SubjectLine = "Business Oportunity",
+			Body = "This is some really long text",
+			IsFavorite = true,
+			IsRead = true,
+		});
+	}
+
+	public BindableCollection<EmailModel> Emails
+	{
+		get { return _emails; }
+		set { _emails = value; }
+	}
+
 }
