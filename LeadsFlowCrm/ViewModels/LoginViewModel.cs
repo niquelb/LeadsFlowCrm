ab130a888ddd4ad2f8ed4b9fe16323a769d7d95f
N P <nicolas.palaorocort03@gmail.com>
2023-05-17 12:23:55 +0200

    Added themes to the app
    Added themes to the app which can be configured in App.xaml

--- LeadsFlowCrm/ViewModels/LoginViewModel.cs
+++ LeadsFlowCrm/ViewModels/LoginViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Caliburn.Micro;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,6 +7,6 @@ using System.Threading.Tasks;
 
 namespace LeadsFlowCrm.ViewModels;
 
-public class LoginViewModel
+public class LoginViewModel : Screen
 {
 }
