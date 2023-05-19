ecc35b38bfca5d19b41c21922260205f58afaa0c
N P <nicolas.palaorocort03@gmail.com>
2023-05-19 10:19:01 +0200

    Update IForeignKeyRelationships.cs

--- DataAccess/DataAccess/IForeignKeyRelationships.cs
+++ DataAccess/DataAccess/IForeignKeyRelationships.cs
@@ -2,6 +2,10 @@
 {
 	public interface IForeignKeyRelationships
 	{
+		Task SetupFieldPipelineRelathionship(string PipelineId, string FieldId);
+		Task SetupOrgCreatorRelationship(string OrgId, string UserId);
+		Task SetupPipelineUserRelationship(string UserId, string PipelineId);
+		Task SetupStagePipelineRelathionship(string PipelineId, string StageId);
 		Task SetupUserOrgRelationship(string UserId, string OrgId);
 	}
 }
