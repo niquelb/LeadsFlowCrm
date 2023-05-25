86445f315cea447a83e8dd23b9ec8737485deabf
N P <nicolas.palaorocort03@gmail.com>
2023-05-25 22:05:22 +0200

    Implemented loading indicator to the InboxView

--- LeadsFlowCrm/ViewModels/InboxViewModel.cs
+++ LeadsFlowCrm/ViewModels/InboxViewModel.cs
@@ -13,12 +13,12 @@ namespace LeadsFlowCrm.ViewModels;
 public class InboxViewModel : Screen
 {
 	private readonly IGmailServiceClass _gmailService;
+
 	private ObservableCollection<Email> _inbox = new();
 
     public InboxViewModel(IGmailServiceClass gmailService)
     {
 		_gmailService = gmailService;
-		Inbox = new ObservableCollection<Email>();
 	}
 
 	protected async override void OnViewLoaded(object view)
@@ -26,8 +26,22 @@ public class InboxViewModel : Screen
 		base.OnViewLoaded(view);
 
 		Inbox = new ObservableCollection<Email>(await _gmailService.GetInboxAsync());
+
+		IsLoading = false;
 	}
-	
+
+	/*
+	 * This property is used to control the visibility of the loading spinner and the inbox ListView
+	 */
+	private bool _isLoading = true;
+	public bool IsLoading
+	{
+		get { return _isLoading; }
+		set { 
+			_isLoading = value;
+			NotifyOfPropertyChange();
+		}
+	}
 
 	public ObservableCollection<Email> Inbox
 	{
