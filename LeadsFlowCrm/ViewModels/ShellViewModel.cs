3afd12f06c5ef902b06cbc59828767f899f20a6e
N P <nicolas.palaorocort03@gmail.com>
2023-05-25 23:39:45 +0200

    Wired up ShellView to handle selected email event
    Added an event listener to the shell view to listen to when the user selects an email

--- LeadsFlowCrm/ViewModels/ShellViewModel.cs
+++ LeadsFlowCrm/ViewModels/ShellViewModel.cs
@@ -1,20 +1,29 @@
 ﻿using Caliburn.Micro;
+using LeadsFlowCrm.EventModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace LeadsFlowCrm.ViewModels;
 
-public class ShellViewModel : Conductor<object>
+public class ShellViewModel : Conductor<object>, IHandle<EmailSelectedEvent>
 {
 	private readonly InboxViewModel _inboxViewModel;
+	private readonly IEventAggregator _event;
 
-	public ShellViewModel(InboxViewModel inboxViewModel)
+	public ShellViewModel(InboxViewModel inboxViewModel, IEventAggregator @event)
     {
 		_inboxViewModel = inboxViewModel;
+		_event = @event;
+		_event.SubscribeOnUIThread(this);
 
 		ActivateItemAsync(_inboxViewModel);
+	}
+
+	public async Task HandleAsync(EmailSelectedEvent e, CancellationToken cancellationToken)
+	{
 	}
 }
