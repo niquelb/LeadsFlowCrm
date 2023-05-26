a5416baf92d16762c11a972a0fd9a6c30dd53d68
N P <nicolas.palaorocort03@gmail.com>
2023-05-26 14:45:40 +0200

    Implemented navigation in SidebarVM

--- LeadsFlowCrm/ViewModels/SidebarViewModel.cs
+++ LeadsFlowCrm/ViewModels/SidebarViewModel.cs
@@ -1,12 +1,33 @@
 ﻿using Caliburn.Micro;
+using LeadsFlowCrm.EventModels;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace LeadsFlowCrm.ViewModels;
 
 public class SidebarViewModel : Screen
 {
+	private readonly IEventAggregator _event;
+
+	public SidebarViewModel(IEventAggregator @event)
+    {
+		_event = @event;
+	}
+
+    public async void Inbox()
+	{
+		await _event.PublishOnUIThreadAsync(new NavigationEvent(NavigationEvent.NavigationRoutes.Inbox));
+	}
+	public async void Pipelines()
+	{
+		throw new NotImplementedException();
+	}
+	public async void Contacts()
+	{
+		throw new NotImplementedException();
+	}
+	public async void Utils()
+	{
+		throw new NotImplementedException();
+	}
 }
