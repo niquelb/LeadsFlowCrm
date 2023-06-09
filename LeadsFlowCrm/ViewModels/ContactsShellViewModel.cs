6d0c0e3f90e59173cb2b730b7b5fdc3966b60b0c
N P <nicolas.palaorocort03@gmail.com>
2023-06-09 22:30:22 +0200

    Implemented the tab navigation and created MyContacts screen
    Implemented the tab navigation using Caliburn Micro's screens and created MyContacts view and viewmodel

--- LeadsFlowCrm/ViewModels/ContactsShellViewModel.cs
+++ LeadsFlowCrm/ViewModels/ContactsShellViewModel.cs
@@ -1,7 +1,24 @@
 ﻿using Caliburn.Micro;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace LeadsFlowCrm.ViewModels;
 
-public class ContactsShellViewModel : Screen
+public class ContactsShellViewModel : Conductor<object>.Collection.OneActive
 {
+	private readonly MyContactsViewModel _myContacts;
+
+	public ContactsShellViewModel(MyContactsViewModel myContacts)
+    {
+		_myContacts = myContacts;
+	}
+
+	protected async override Task OnInitializeAsync(CancellationToken cancellationToken)
+	{
+		await base.OnInitializeAsync(cancellationToken);
+
+		Items.Add(_myContacts);
+
+		await ActivateItemAsync(_myContacts, cancellationToken);
+	}
 }
---
+++ LeadsFlowCrm/ViewModels/MyContactsViewModel.cs
@@ -0,0 +1,77 @@
+﻿using Caliburn.Micro;
+using LeadsFlowCrm.EventModels;
+using System;
+using System.ComponentModel;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LeadsFlowCrm.ViewModels;
+
+public class MyContactsViewModel : Screen
+{
+    public MyContactsViewModel()
+    {
+    }
+
+	protected async override Task OnActivateAsync(CancellationToken cancellationToken)
+	{
+		await base.OnActivateAsync(cancellationToken);
+
+		ShowLoadingScreen = true;
+
+		await Task.Delay(1000, cancellationToken);
+
+		ShowLoadingScreen = false;
+		ShowEmptyScreen = true;
+
+		await Task.Delay(1000, cancellationToken);
+
+		ShowEmptyScreen = false;
+		ShowContent = true;
+	}
+
+	public string DisplayHeader { get; set; } = "My Contacts";
+
+    /*
+	 * Private backing fields
+	 */
+    private bool _showContent;
+	private bool _showEmptyScreen;
+	private bool _showLoadingScreen;
+
+	/// <summary>
+	/// Controls wether the main content is showing or not
+	/// </summary>
+	public bool ShowContent
+	{
+		get { return _showContent; }
+		set { 
+			_showContent = value;
+			NotifyOfPropertyChange();
+		}
+	}
+
+	/// <summary>
+	/// Controls wether the loading screen is showing or not
+	/// </summary>
+	public bool ShowLoadingScreen
+	{
+		get { return _showLoadingScreen; }
+		set { 
+			_showLoadingScreen = value;
+			NotifyOfPropertyChange();
+		}
+	}
+
+	/// <summary>
+	/// Controls wether the "empty" screen is showing or not
+	/// </summary>
+	public bool ShowEmptyScreen
+	{
+		get { return _showEmptyScreen; }
+		set {
+			_showEmptyScreen = value;
+			NotifyOfPropertyChange();
+		}
+	}
+}
