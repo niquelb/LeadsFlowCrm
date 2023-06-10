c9c8fb8dce7ddbfed6aac23a3fad15e6d294526e
N P <nicolas.palaorocort03@gmail.com>
2023-06-10 22:49:18 +0200

    Implemented autocomplete for sending emails
    Implemented autocomplete functionality for the "To" field when sending emails which reads the email addresses from the contacts that are associated with the user

--- LeadsFlowCrm/ViewModels/CreateDraftViewModel.cs
+++ LeadsFlowCrm/ViewModels/CreateDraftViewModel.cs
@@ -12,6 +12,8 @@ using System.Windows.Documents;
 using System.Windows.Controls;
 using Notifications.Wpf.Controls;
 using Notifications.Wpf;
+using System.Threading;
+using LeadsFlowCrm.Services.ModelServices;
 
 namespace LeadsFlowCrm.ViewModels;
 
@@ -19,11 +21,35 @@ public class CreateDraftViewModel : Screen
 {
 
 	private readonly IGmailServiceClass _gmailService;
+	private readonly IContactService _contactService;
+	private readonly LoggedInUser _loggedInUser;
 
-    public CreateDraftViewModel(IGmailServiceClass gmailService)
+	public CreateDraftViewModel(IGmailServiceClass gmailService,
+							 IContactService contactService,
+							 LoggedInUser loggedInUser)
     {
 		_gmailService = gmailService;
+		_contactService = contactService;
+		_loggedInUser = loggedInUser;
 	}
+
+	protected async override Task OnInitializeAsync(CancellationToken cancellationToken)
+	{
+		await base.OnInitializeAsync(cancellationToken);
+
+		// We retrieve the contacts from the API
+		Contacts = (await _contactService.GetAllFromUserAsync(_loggedInUser.Id)).ToList();
+
+		/*
+		 * We iterate through these contacts and add the email to the AutoCompleteData collection.
+		 * 
+		 * This allows for the autocompletion of the TextBox with the contact's emails.
+		 */
+        foreach (var contact in Contacts)
+        {
+			AutoCompleteData.Add(contact.Email);
+        }
+    }
 
 	/// <summary>
 	/// Method for showing a "toast" style notification to the user
@@ -39,6 +65,9 @@ public class CreateDraftViewModel : Screen
 			Type = notificationType
 		});
 
+	/// <summary>
+	/// Method to send the email
+	/// </summary>
 	public async void Send()
 	{
 		if (string.IsNullOrWhiteSpace(To) || string.IsNullOrWhiteSpace(SubjectLine) || string.IsNullOrWhiteSpace(Body))
@@ -78,17 +107,22 @@ public class CreateDraftViewModel : Screen
 		await TryCloseAsync();
 	}
 
+	/// <summary> Notification manager for the toast-style notifications </summary>
 	public NotificationManager Notification { get; set; } = new();
 
-	/*
+	/// <summary> Message that will be sent/drafted </summary>
+	public GmailApi.Message Msg { get; set; } = new();
+
+	/// <summary> Contacts that "belong" to the user from the API </summary>
+	public List<Contact> Contacts { get; set; } = new();
+
+    /*
 	 * Prop backing fields
 	 */
 	private string _to = string.Empty;
 	private string _subjectLine = string.Empty;
 	private string _body = string.Empty;
-
-	/// <summary> Message that will be sent/drafted </summary>
-	public GmailApi.Message Msg { get; set; } = new();
+	private BindableCollection<string> _autoCompleteData = new();
 
 	/// <summary>
 	/// Reciever
@@ -126,5 +160,16 @@ public class CreateDraftViewModel : Screen
 		}
 	}
 
+	/// <summary>
+	/// A listto be used for the autofill of the "to" field
+	/// </summary>
+	public BindableCollection<string> AutoCompleteData
+	{
+		get { return _autoCompleteData; }
+		set { 
+			_autoCompleteData = value;
+			NotifyOfPropertyChange();
+		}
+	}
 
 }
