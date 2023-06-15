b03ca201a48e58fc204e97ed800f82bf874a77e2
N P <nicolas.palaorocort03@gmail.com>
2023-06-15 23:06:02 +0200

    Implemented missing methods on StageViewModel
    Implemented the missing methods for the stage screen

--- LeadsFlowCrm/ViewModels/StageViewModel.cs
+++ LeadsFlowCrm/ViewModels/StageViewModel.cs
@@ -1,9 +1,12 @@
 ﻿using Caliburn.Micro;
+using Google.Apis.PeopleService.v1.Data;
 using LeadsFlowCrm.EventModels;
 using LeadsFlowCrm.Models;
 using LeadsFlowCrm.Services.ModelServices;
 using LeadsFlowCrm.Utils;
 using Notifications.Wpf;
+using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Linq;
@@ -15,11 +18,19 @@ namespace LeadsFlowCrm.ViewModels;
 public class StageViewModel : Screen, IHandle<StageSelectedEvent>
 {
 	private readonly IContactService _contactService;
+	private readonly IEventAggregator _event;
+	private readonly IWindowManager _windowManager;
+	private readonly CreateDraftViewModel _createDraft;
 
 	public StageViewModel(IContactService contactService,
-					   IEventAggregator @event)
+					   IEventAggregator @event,
+					   IWindowManager windowManager,
+					   CreateDraftViewModel createDraft)
     {
 		_contactService = contactService;
+		_event = @event;
+		_windowManager = windowManager;
+		_createDraft = createDraft;
 		@event.SubscribeOnUIThread(this);
 	}
 
@@ -40,6 +51,7 @@ public class StageViewModel : Screen, IHandle<StageSelectedEvent>
 	#endregion
 
 	#region Public Methods
+
 	/// <summary>
 	/// Method to refresh the contacts list
 	/// </summary>
@@ -50,9 +62,47 @@ public class StageViewModel : Screen, IHandle<StageSelectedEvent>
 		await LoadContacts();
 	}
 
+	/// <summary>
+	/// Method for creating a new draft with the selcted contact's email as the recipient
+	/// </summary>
+	/// <param name="contact">Selected contact</param>
+	public async void NewDraft(Contact contact)
+	{
+		await _windowManager.ShowWindowAsync(_createDraft);
+
+		await _event.PublishOnUIThreadAsync(new DraftEvent()
+		{
+			Recipients = new List<string>() {
+				contact.Email
+			}
+		});
+	}
+
+	/// <summary>
+	/// Method from removing the selected contact from the stage
+	/// </summary>
+	/// <param name="contact">Selected contact</param>
+	public async void Remove(Contact contact)
+	{
+		contact.StageId = string.Empty;
+
+		Utilities.ShowNotification("Removing contact from stage", "Removing the selected contact from the stage...", NotificationType.Information);
+		try
+		{
+			await _contactService.ModifyStageIdToApiAsync(contact.Id, "887b03e7-3037-4e14-a906-ca0424aee547"); // TODO DO NOT DO THIS, this is a temporary fix but it is manually moving the contact to a stage in another pipeline
+			Contacts.Remove(contact);
+			Utilities.ShowNotification("Removed contact from stage", "Successfully removed the contact from the stage.", NotificationType.Success);
+		}
+		catch (Exception ex)
+		{
+			Utilities.ShowNotification("Failed removing process", $"Failed to remove the contact from the stage. ({ex.Message})", NotificationType.Error);
+		}
+	}
+
 	#endregion
 
 	#region Private Methods
+
 	/// <summary>
 	/// Method that loads the Contacts collection from the API and shows/hides the "empty" screen
 	/// </summary>
@@ -82,12 +132,8 @@ public class StageViewModel : Screen, IHandle<StageSelectedEvent>
 	/// </summary>
 	public Stage Stage { get; set; } = new();
 
-	/// <summary>
-	/// Selected contact
-	/// </summary>
-	public Contact? SelectedContact { get; set; }
-
 	#region Property backing fields
+
 	private bool _emptyScreenIsVisible;
 	private bool _contentIsVisible;
 
